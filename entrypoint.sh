6eff21d794a0e30a4755018ad5cebe8bb380d815
Valmir Barbosa <valmir.php@gmail.com>
2022-04-01 14:00:50 -0300

    feat: permitir configmap

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,17 @@ set -e
 export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
+if [ ! -z "$INPUT_K8S_FILE_CONFIG" ]; then
+  echo "Print >> $INPUT_K8S_FILE_CONFIG"
+  echo "FILE: /k8s-config.yaml"
+  echo $INPUT_K8S_FILE_CONFIG > /k8s-config.yaml
+fi
+
 echo "aws version"
 
 aws --version
 
 echo "Attempting to update kubeconfig for aws"
 
-aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
-kubectl "$@"
+#aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+#kubectl "$@"
