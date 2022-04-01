fcd545b1bd425555d4208596203d1ae6833ebcfa
Valmir Barbosa <valmir.php@gmail.com>
2022-04-01 14:31:13 -0300

    feat: permitir configmap

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,5 +17,5 @@ aws --version
 
 echo "Attempting to update kubeconfig for aws"
 
-#aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
-#kubectl "$@"
+aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+kubectl "$@"
